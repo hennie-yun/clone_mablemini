deb8ba966a07b764088231cf64d216e37a56ed8b
hennie <hennie9401@gmail.com>
2024-07-01 11:06:06 +0900

    zzimPage 초기 화면

--- lib/manager/GlobalController.dart
+++ lib/manager/GlobalController.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
+import '../page/zzim.dart';
+
 class GlobalController extends GetxController {
   static GlobalController get instance => Get.find();
 
   var selectedIndex = 0.obs;
-  var currentWidget = Rx<Widget>(Container()); // 초기화
+  var currentWidget = Rx<Widget>(zzim()); // 초기화
 
   void setCurrWidget(Widget widget) {
     currentWidget.value = widget;
