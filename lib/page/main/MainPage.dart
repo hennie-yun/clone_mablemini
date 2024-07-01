9db74b4709bba102f80638b5368073888110e973
hennie <hennie9401@gmail.com>
2024-07-01 14:58:35 +0900

    appBar 및 morepage

--- lib/page/main/MainPage.dart
+++ lib/page/main/MainPage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-
 
 import '../../manager/GlobalController.dart';
 import '../more/MorePage.dart';
@@ -9,47 +7,42 @@ import '../price/PricePage.dart';
 import '../zzim.dart';
 
 class MainPage extends StatelessWidget {
-
-
   final GlobalController _globalCtrl = Get.find<GlobalController>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   MainPage({super.key});
-
-
-
 
   Widget setBottom() {
     return Obx(() {
       return BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
-              icon: _globalCtrl.selectedIndex.value == 0 ?
-              Icon(Icons.star_rounded, color: Colors.yellow) :
-              Icon(Icons.star_outline_rounded, color: Colors.black),
-              label: 'fav'),
-          BottomNavigationBarItem(
-              icon: _globalCtrl.selectedIndex.value == 1 ? Icon(
-                Icons.insert_chart_rounded, color: Colors.yellow,) : const Icon(
-                Icons.insert_chart_outlined_rounded , color: Colors.black,),
-              label: 'price'
+            icon: _globalCtrl.selectedIndex.value == 0
+                ? Icon(Icons.star_rounded, color: Colors.yellow)
+                : Icon(Icons.star_outline_rounded, color: Colors.black),
+            label: 'fav',
           ),
           BottomNavigationBarItem(
-              icon: _globalCtrl.selectedIndex.value == 2 ? Icon(
-                Icons.more_horiz_rounded , color: Colors.yellow,) : Icon(
-                  Icons.more_horiz_rounded , color: Colors.black),
-              label: 'more'
+            icon: _globalCtrl.selectedIndex.value == 1
+                ? Icon(Icons.insert_chart_rounded, color: Colors.yellow)
+                : Icon(Icons.insert_chart_outlined_rounded,
+                    color: Colors.black),
+            label: 'price',
+          ),
+          BottomNavigationBarItem(
+            icon: _globalCtrl.selectedIndex.value == 2
+                ? Icon(Icons.more_horiz_rounded, color: Colors.yellow)
+                : Icon(Icons.more_horiz_rounded, color: Colors.black),
+            label: 'more',
           ),
         ],
-
         currentIndex: _globalCtrl.selectedIndex.value,
         selectedItemColor: Colors.black,
-
         onTap: (value) {
           _globalCtrl.selectedIndex.value = value;
           switch (value) {
             case 0:
-               _globalCtrl.setCurrWidget(zzim()); // 찜하기 페이지
+              _globalCtrl.setCurrWidget(zzim()); // 찜하기 페이지
               break;
             case 1:
               _globalCtrl.setCurrWidget(PricePage());
@@ -66,11 +59,70 @@ class MainPage extends StatelessWidget {
     });
   }
 
+  Widget setAppBar() {
+    return Obx(() {
+    String titleWidget ='';
+    List<Widget> actions = [];
+
+     switch (_globalCtrl.selectedIndex.value) {
+       case 0:
+         titleWidget = '찜한주식';
+         actions = [
+           IconButton(
+             icon: Icon(Icons.add),
+             onPressed: () {},
+           ),
+           IconButton(
+             icon: Icon(Icons.create_outlined),
+             onPressed: () {},
+           ),
+         ];
+         break;
+       case 1:
+         titleWidget = '';
+         actions = [
+           IconButton(
+             icon: Icon(Icons.notification_add_outlined),
+             onPressed: () {},
+           ),
+           IconButton(
+             icon: Icon(Icons.share),
+             onPressed: () {},
+           ),
+         ];
+         break;
+       case 2:
+         titleWidget = '더보기';
+         actions = [
+           IconButton(
+             icon: Icon(Icons.settings),
+             onPressed: () {},
+           ),
+         ];
+         break;
+     }
+
+    return AppBar(
+        leading : titleWidget == '' ? IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {
+          },
+        ) : null,
+      title: Text(titleWidget),
+      actions: actions,
+    );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       key: _scaffoldKey,
+      appBar:  PreferredSize(
+        preferredSize: const Size(double.infinity, 64),
+          child :  setAppBar()
+      ),
       body: SafeArea(
         child: Obx(() {
           return _globalCtrl.currentWidget.value;
--- lib/page/more/MorePage.dart
+++ lib/page/more/MorePage.dart
@@ -1,17 +1,214 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MorePage extends StatelessWidget {
+  const MorePage({Key? key}) : super(key: key);
 
   @override
-  Widget  build (BuildContext context) {
-    return Scaffold (
-      body : Container(
-       child: Text( "더보기페이지")
-      )
-
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildUserInfoContainer(),
+            SizedBox(height: 16),
+            _buildSection(
+              title: '배당 및 세금',
+              items: [
+                '해외주식 배당내역',
+                '해외주식 배당옵션',
+                '양도소득세 조회/신청',
+              ],
+            ),
+            SizedBox(height: 16),
+            _buildSection(
+              title: '뱅킹',
+              items: [
+                '이체하기',
+                '이체내역',
+                '거래내역',
+                '충전하기',
+                '환전하기',
+                '환전신청내역',
+                '충전계좌관리',
+                '이체한도관리',
+                '간편이체 등록/해지',
+                '출고 신청/내역',
+              ],
+            ),
+            SizedBox(height: 16),
+            _buildSection(
+              title: '내 주식 관리',
+              items: [
+                '나의 정기구매',
+                '정기구매 내역',
+                '주식 선물하기',
+                '온주전환',
+              ],
+            ),
+            SizedBox(height: 16),
+            _buildSection(
+              title: '프라임 클럽',
+              items: [
+                'MY 프라임',
+                '증권방송 쪽지함',
+                '프라임 상담톡',
+              ],
+            ),
+            SizedBox(height: 16),
+            _buildSection(
+              title: '내 정보 관리',
+              items: [
+                '투자자정보 등록/조회',
+                '개인(신용)정보동의서',
+                '내 수수료 조회',
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 
+  Widget _buildUserInfoContainer() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16, 24, 16, 24),
+      decoration: BoxDecoration(
+        color: Color(0XFFF4F5F8),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: 160,
+      child : Column(
+      children : [
+        Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Row(
+            children: [
+              Text(
+                '김국민님',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: Color(0XFFB2B4BF),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 1),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 1, color: Colors.grey.withOpacity(0.3)),
+            ),
+            child: Text('로그아웃'),
+          ),
+        ],
+      ),
+      SizedBox(height: 16),
+      _buildIconWithDescriptions(),
+      ]),
+    );
+  }
+
+  Widget _buildIconWithDescriptions() {
+    return Container(
+      height: 66,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildIconWithDescription(Icons.lock, '인증/보안', Color(0XFFFB422E)),
+          _buildIconWithDescription(Icons.card_giftcard, '선물함', Color(0XFF8C52EF)),
+          _buildIconWithDescription(Icons.local_offer, '쿠폰함', Color(0XFF49D0C7)),
+          _buildIconWithDescription(Icons.emoji_events, '이벤트', Color(0XFFFFDA80)),
+          _buildIconWithDescription(Icons.headphones_outlined, '고객서비스', Color(0XFF3B60FF)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIconWithDescription(IconData icon, String text, Color color) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(14),
+          child: Icon(
+            icon,
+            size: 20,
+            color: color,
+          ),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
 }
+
+Widget _buildSection({required String title, required List<String> items}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 52,
+          padding: EdgeInsets.fromLTRB(24, 16, 24, 16),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+              color: Color(0XFFE58832),
+            ),
+          ),
+        ),
+        Column(
+          children: items.map((item) => _buildDetailContent(item)).toList(),
+        ),
+        SizedBox(height: 16),
+        Divider(
+          height: 1,
+          color: Colors.grey.withOpacity(0.3),
+          thickness: 1,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDetailContent(String content) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: 52,
+        padding: EdgeInsets.fromLTRB(24, 14, 24, 14),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              content,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+            ),
+            const Icon(
+              Icons.arrow_forward_ios,
+              color: Color(0XFFB2B4BF),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
